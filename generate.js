4b86389b53d6c726a74ad7919985dd4595c64054
Stephen Woods <stephen.woods@salesforce.com>
2018-06-17 10:41:39 -0700

    support multiple electeds

--- generate.js
+++ generate.js
@@ -2,18 +2,31 @@ const showdown = require('showdown');
 const fs = require('fs-extra');
 const path = require('path');
 const ejs = require('ejs');
-
-
 const converter = new showdown.Converter({metadata: true});
-
+const electeds = JSON.parse(fs.readFileSync('data/electeds.json', {encoding:'utf8'}));
+console.log(electeds);
 const issues = fs.readdirSync('./issues');
-// const html = converter.makeHtml(detention);
-// converter.getMetadata();
+const scripts = fs.readdirSync('./scripts');
 let issueData = {};
 const issueList = [];
+const scriptList = [];
+
+// async, but has no callback
+function render(tmpl, data, path) {
+    ejs.renderFile(tmpl, data, (err, str) => {
+        if(err) {
+            console.error(err);
+        } else {
+            fs.outputFile(path, str).catch(err => {
+                console.error(err);
+            });
+        }
+        
+    });
+}
 
 issues.forEach(issue => {
-    const html = converter.makeHtml(fs.readFileSync(path.join(__dirname,'issues', issue),{encoding:'utf8'}));
+    const html = converter.makeHtml(fs.readFileSync(path.join(__dirname,'issues',issue),{encoding:'utf8'}));
     const out = converter.getMetadata();
     out.html = html;
     issueData[issue] = out;
@@ -21,12 +34,23 @@ issues.forEach(issue => {
     issueList.push(out);
 });
 
+scripts.forEach(script => {
+    const html = converter.makeHtml(fs.readFileSync(path.join(__dirname,'scripts', script),{encoding:'utf8'}));
+    const scriptdata = converter.getMetadata();
+    scriptdata.html = html;
+    scriptdata.name = script.replace(/\.md/,'');
+    scriptdata.issueHtml = issueData[scriptdata.issue + '.md'].html;
+    scriptList.push(scriptdata);
+    render('src/scripthome.ejs', {scriptdata, electeds}, `./dist/${scriptdata.name}/index.html`);
 
-
-
-
-
-ejs.renderFile('src/index.ejs',{issueList}, (err, str) => {
+    electeds.forEach(elected => {
+        let name = elected.lastName.toLowerCase();
+        scriptdata.html = scriptdata.html.replace('{{full_name}}', `${elected.salutation} ${elected.lastName}`);
+        render('src/script.ejs', {scriptdata, elected}, `./dist/${scriptdata.name}/${name}/index.html`);
+    });
+    
+});
+ejs.renderFile('src/index.ejs',{issueList, scriptList, electeds}, (err, str) => {
     if(err) {
         console.error(err);
     } else {
