ac1ecc9ec859e57060f1e85edf84a135440aaf72
Stephen Woods <stephen.woods@salesforce.com>
2018-06-17 13:45:36 -0700

    port configure

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var connect = require('connect');
 var path = require('path');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(path.join(__dirname, '/dist'))).listen(5000, function(){
-    console.log('Listening on 5000');
+var port = process.env.PORT;
+connect().use(serveStatic(path.join(__dirname, '/dist'))).listen(port, function(){
+    console.log('Listening on '+ port);
 });
