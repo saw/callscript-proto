7349922298f246f505f71a3d26797ff67f683e9d
Stephen Woods <stephen.woods@salesforce.com>
2018-06-17 13:48:40 -0700

    port stuff

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var connect = require('connect');
 var path = require('path');
 var serveStatic = require('serve-static');
-var port = process.env.PORT;
+var port = process.env.WEB_PORT;
 connect().use(serveStatic(path.join(__dirname, '/dist'))).listen(port, function(){
     console.log('Listening on '+ port);
 });
